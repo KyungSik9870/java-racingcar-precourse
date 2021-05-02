3927a0e81c62eef72210c63532450584706e5aa2
KyungSik9870 <rudtlr9870@gmail.com>
2021-05-02 23:02:23 +0900

    feat: 자동차들을 가지는 객체 생성.

---
+++ src/main/java/com/nextstep/precourse/racingcar/Cars.java
@@ -0,0 +1,16 @@
+package com.nextstep.precourse.racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+	List<Car> cars;
+
+	public Cars(String carNamesString) {
+		this.cars = new ArrayList<>();
+		CarNames carNames = new CarNames(carNamesString);
+		for (CarName carName : carNames.getCarNames()) {
+			this.cars.add(new Car(carName));
+		}
+	}
+}
